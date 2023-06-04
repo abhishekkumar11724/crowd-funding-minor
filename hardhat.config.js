d25773522cf06bb068d97a425eb482379cf75cc8
abhishek kumar <82565067+abhishekkumar11724@users.noreply.github.com>
2023-06-04 16:19:45 +0530

    made the contract live

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,11 @@
 require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-waffle");
-// const { ACCOUNT_PRIVATE_KEY,ALCHEMY_KEY } = process.env;
+const ACCOUNT_PRIVATE_KEY = "4c9dedc7ff51678e7dc4798b97a9e9039c3bfb6c02b5b6e0eb65648bcf8d74b9";
+const ALCHEMY_KEY = "9CoodifaspeMuch2RepgIqrclyXhAk73";
 
 module.exports = {
   solidity: "0.8.4",
-  // defaultNetwork: "rinkeby",
+  defaultNetwork: "sepolia",
   paths: {
     artifacts: "./client/artifacts",
   },
@@ -13,9 +14,9 @@ module.exports = {
       chainId: 31337,
       // allowUnlimitedContractSize: true
     },
-    // rinkeby: {
-    //   url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_KEY}`,
-    //   accounts: [`0x${ACCOUNT_PRIVATE_KEY}`]
-    // }
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_KEY}`,
+      accounts: [`0x${ACCOUNT_PRIVATE_KEY}`]
+    }
   },
 };
